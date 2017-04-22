5007afe0a8dcf62fd912d4ac8aa5c66cc818416e
biezhihua <448326497@qq.com>
2017-04-22 21:43:04 +0800

    update

---
+++ FFmpeg纯净解码器/cmake-build-debug/CMakeFiles/FFmpeg.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FFmpeg.dir/main.c.o"
+  "FFmpeg.pdb"
+  "FFmpeg"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/FFmpeg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
